7b6ac427a520c0b7b3f944ef5fd1a1afca0e92c1
profstein <cstein@bmcc.cuny.edu>
2022-04-02 22:05:59 +0000

    adding css and js files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-// You can include your own javascript here
-// alert("hello, your main.js file was added properly.");
-console.log("Logging from main.js, it was loaded.");
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
